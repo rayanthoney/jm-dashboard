0578635ecdc347fe6b725a6d1e8040f32e380962
Ray Anthoney <rmaxey911@gmail.com>
2023-03-07 21:04:04 -0700

    added Properties page

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,6 @@ function App() {
           return Promise.reject();
         }
       }
-
       localStorage.setItem("token", `${credential}`);
 
       return Promise.resolve();
@@ -129,7 +128,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          dataProvider={dataProvider("http://localhost:8080/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
